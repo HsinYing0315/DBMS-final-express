65041fcb3f692e6c3152616522da547d1a1f9d61
Melody Wu <110306066@g.nccu.edu.tw>
2023-05-23 13:22:25 +0800

    feat: item model

--- model/item.model.js
+++ model/item.model.js
@@ -1,5 +1,33 @@
 const { connection } = require("../database/db");
 
+const addItem = async (data) => {
+  console.log("add new item")
+  const keys = Object.keys(data)
+  const values = Object.values(data)
+
+  let keyString = keys[0]
+  let valueString = "'" + values[0] + "'"
+  for(let i = 1; i < keys.length; i ++) {
+    keyString  += ", " + keys[i]
+    valueString += ", '" + values[i] + "'"
+  }
+
+  const now = new Date()
+  const YY = now.getFullYear()
+  const MM = now.getMonth() < 10 ? "0" + now.getMonth() : now.getMonth()
+  const DD = now.getDay() < 10 ? "0" + now.getDay() : now.getDay()
+  const hh = (now.getHours() < 10 ? "0" + now.getHours() : now.getHours()) + ":";
+  const mm = (now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()) + ":";
+  const ss = now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+  const nowTime = YY + "-" + MM + "-" + DD + " " + hh + mm + ss
+
+  await connection.query(`INSERT INTO item (${keyString}, startDate) VALUES (${valueString}, '${nowTime}');`, (err, res) => {
+    if (err) {
+        console.error(err);
+    }
+  });
+}
 const getItems = async () => {
   console.log("fetch all items")
   
@@ -15,7 +43,33 @@ const getItems = async () => {
 
   return rows
 }
+const updItem = async (id, data) => {
+  console.log("item updated")
+  const keys = Object.keys(data)
+  const values = Object.values(data)
 
-module.exports = { 
-    getItems
+  let updString = keys[0] + "=" + "'" + values[0] + "'"
+  for(let i = 1; i < keys.length; i ++) {
+    updString += "," + keys[i] + "=" + "'" + values[i] + "'"
+  }
+
+  await connection.query(`UPDATE item SET ${updString} WHERE id = '${id}';`, (err, res) => {
+    if (err) {
+        console.error(err)
+    }
+  })
+}
+const delItem = async (id) => {
+  await connection.query(`DELETE FROM item WHERE id = ${id}`, (err, result) => {
+    if(err) {
+      console.error(err)
+    }
+  })
+}
+
+module.exports = {
+  addItem, 
+  getItems,
+  updItem,
+  delItem
 }
--- routes/item.route.ts
+++ routes/item.route.ts
@@ -1,12 +1,58 @@
-const express = require("express")
-const router = express.Router()
-const { getItems } = require("../model/item.model")
+const itemExpress = require("express")
+const router = itemExpress.Router()
+const { addItem, getItems, updItem, delItem } = require("../model/item.model")
 
-router.get("/", async (req, res) => {
-    const result = await getItems()
-
-	res.status(200).send({
-		msg: result
-	})
+router.post("/", async (req, res) => {
+    const data = req.body
+	try {
+		await addItem(data)
+		res.status(201).send({
+			msg: data,
+		})
+	} catch (error) {
+		res.status(500).send({
+			msg: "Internal Server Error"
+		})
+	}
 })
+router.get("/", async (req, res) => {
+    try {
+        const items = await getItems()
+        res.status(200).send({
+            data: items
+        })
+    } catch (error){
+        res.status(500).send({
+			msg: "Internal Server Error"
+		})
+    }
+
+})
+router.patch("/:id", async (req, res) => {
+    const data = req.body
+    try {
+        await updItem(req.params.id, data)
+        res.status(200).send({
+            msg: "item updated"
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({
+            msg: "Internal Server Error"
+        })
+    }
+})
+router.delete("/:id", async (req, res) => {
+    try {
+        await delItem(req.params.id)
+        res.status(200).send({
+            msg: "item deleted"
+        })
+    } catch (err) {
+        res.status(500).send({
+            msg: "Internal Server Error"
+        })
+    }
+})
+
 module.exports = router
